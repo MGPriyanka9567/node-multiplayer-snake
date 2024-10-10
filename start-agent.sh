aff351c39527d614f68c8ed9326001253efde202
MGPriyanka9567 <priya.murukan@gmail.com>
2024-10-10 12:21:09 +0530

    test

---
+++ start-agent.sh
@@ -0,0 +1,2 @@
+
+nohup java -jar agent.jar -url http://192.168.56.101/ -secret 87bc464ca8d6361f9cad90fb47e34f420880f693a3f1ee19c709422ff03e7993 -name ubuntu -workDir "/home/priyanka/Desktop/app-test/node-multiplayer-snake" &
